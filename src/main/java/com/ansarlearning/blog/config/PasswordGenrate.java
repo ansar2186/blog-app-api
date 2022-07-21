2d53045de9d43fa1b2b32ba1c95c1ba9bc22a54a
ansar ahmad <humzaansari53@gmail.com>
2022-07-21 18:25:09 +0530

    configurations changes

--- src/main/java/com/ansarlearning/blog/config/PasswordGenrate.java
+++ src/main/java/com/ansarlearning/blog/config/PasswordGenrate.java
@@ -1,5 +1,7 @@
 package com.ansarlearning.blog.config;
 
+
+//this class used for testing perpose only
 public class PasswordGenrate {
 
 }
--- src/main/java/com/ansarlearning/blog/config/ProfileConfig.java
+++ src/main/java/com/ansarlearning/blog/config/ProfileConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+//this class used for testing perpose only
+
 @Configuration
 public class ProfileConfig {
 	
