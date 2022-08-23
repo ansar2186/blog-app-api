62c181eb57cae59f28e6c81d5395c15167322b40
ansar ahmad <humzaansari53@gmail.com>
2022-08-23 19:20:11 +0530

    Write some Junit test cases

--- src/test/java/com/ansarlearning/blog/BlogAppApiApplicationTests.java
+++ src/test/java/com/ansarlearning/blog/BlogAppApiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.ansarlearning.blog;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
@@ -9,13 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ansarlearning.blog.entity.Post;
+import com.ansarlearning.blog.entity.User;
 import com.ansarlearning.blog.repository.PostRepository;
+import com.ansarlearning.blog.repository.UserRepository;
 
 @SpringBootTest
 class BlogAppApiApplicationTests {
 	
 	@Autowired
 	public PostRepository postRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
@@ -25,9 +31,29 @@ class BlogAppApiApplicationTests {
 	@Test
 	public void findPostTestCase() {
 		List<Post> findAll = this.postRepository.findAll();
-		assertThat(findAll).size().isGreaterThan(0);
+		assertThat(findAll).size().isPositive();
+		
+	}
+	
+	@Test
+	void testUserCreate() {
+		User user= new User();
+		//user.setId(105);
+		user.setEmail("Johan.pitter@dxc.com");
+		user.setName("Johan");
+		user.setAbout("IT");
+		user.setPassword("johan123");
+		userRepository.save(user);	
+		assertNotNull(userRepository.findUserByEmail("Johan.pitter@dxc.com").getClass());
 		
 	}
 
+	@Test
+	void testFindAllUser() {
+		
+		List<User> findAll = userRepository.findAll();
+		assertThat(findAll).size().isPositive();
+		
+	}
 
 }
